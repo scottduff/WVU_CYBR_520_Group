054526389c9e8403aa10ec96d1ad0befe7dba851
Scott Duff <austin.duff@icloud.com>
2022-11-05 19:12:26 -0400

    Code Changes

--- CIC_IDS_2017_Final/Scott_Duff_Final_Project.R
+++ CIC_IDS_2017_Final/Scott_Duff_Final_Project.R
@@ -1,34 +1,34 @@
 library(caret)
 library(e1071)
-library(ggplot2)
 
 # I have this here for testing and comparison, since I know this works
-#spambase <- read.csv("C:\\Users\\duffa\\OneDrive\\Desktop\\CYBR 520\\Final_Project\\spambase.csv", sep = ",")
+spambase <- read.csv("C:\\Users\\duffa\\OneDrive\\Documents\\GitHub\\WVU_CYBR_520_Group_1\\CIC_IDS_2017_Final\\spambase.csv", sep = ",")
 
-cicids2017 <- read.csv("C:\\Users\\duffa\\OneDrive\\Desktop\\CYBR 520\\Final_Project\\MachineLearningCVE\\Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv", sep = ",")
+cicids2017 <- read.csv("C:\\Users\\duffa\\OneDrive\\Documents\\GitHub\\WVU_CYBR_520_Group_1\\CIC_IDS_2017_Final\\MachineLearningCVE\\Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv", sep = ",")
+
 
 
 # I have this here for testing and comparison, since I know this works
-#spambase$x <- NULL
-
-cic$x <- NULL
+spambase$x <- NULL
+cicids2017$x <- NULL
 
 set.seed(4242)
 
 # I have this here for testing and comparison, since I know this works
-#spamtrainIndex <- createDataPartition(spambase$type, p = 0.70, list = FALSE)
-#spamtrain <- spambase[ spamtrainIndex, ]
-#spamtest <- spambase[-spamtrainIndex, ]
+spambasetrainIndex <- createDataPartition(spambase$type, p = 0.70, list = FALSE)
+spambasetrain <- spambase[ spambasetrainIndex, ]
+spambasetest <- spambase[-spambasetrainIndex, ]
 
-cictrainIndex <- createDataPartition(cicids2017$Label, p = 0.70, list = FALSE)
-cicids2017train <- cic[ cicids2017trainIndex, ]
-cicids2017test <- cic[-cicids2017trainIndex, ]
-
-
+cicids2017trainIndex <- createDataPartition(cicids2017$Label, p = 0.70, list = FALSE)
+cicids2017train <- cicids2017[ cicids2017trainIndex, ]
+cicids2017test <- cicids2017[-cicids2017trainIndex, ]
 
 trainCtrl <- trainControl(method = "cv", number = 10, verboseIter = TRUE)
 
 # I have this here for testing and comparison, since I know this works
-#svm.spamModel <- train(type~., data = spamtrain, method = "svmRadial", tuneLength = 10, trControl = trainCtrl, metric = "Accuracy")
+svm.spambaseModel <- train(type~., data = spambasetrain, method = "svmRadial", tuneLength = 10, trControl = trainCtrl, metric = "Accuracy")
 
 svm.cicids2017Model <- train(Label~., data = cicids2017train, method = "svmRadial", tuneLength = 10, trControl = trainCtrl, metric = "Accuracy")
+
+
+
