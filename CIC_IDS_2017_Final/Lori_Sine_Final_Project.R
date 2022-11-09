6c4b137cb3f15d71e5bca1763437271e15c1416f
lsine1 <lsine1@mix.wvu.edu>
2022-11-09 13:59:07 -0500

    Create Lori_Sine_Final_Project.R

---
+++ CIC_IDS_2017_Final/Lori_Sine_Final_Project.R
@@ -0,0 +1,35 @@
+library(caret)
+library(e1071)
+
+set.seed(4242)
+
+cicids2017 <- read.csv("C:\\Users\\duffa\\OneDrive\\Documents\\GitHub\\WVU_CYBR_520_Group_1\\CIC_IDS_2017_Final\\MachineLearningCVE\\Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv", sep = ",")
+cicids2017$x <- NULL
+
+# Selects Columns that total Zero.
+only_zeros <- function(x) {
+  if(class(x) %in% c("integer", "numeric")) {
+    all(x == 0, na.rm = TRUE) 
+  } else { 
+    FALSE
+  }
+}
+
+# Removes columns that total Zero
+cicids2017RemoveZeroCol <- cicids2017[ , !sapply(cicids2017, only_zeros)]
+
+# The results of this command
+# table(cicids2017RemoveZeroCol$Label)
+# BENIGN   DDoS 
+# 97718    128027 
+
+
+#cicids2017trainIndex <- createDataPartition(cicids2017$Label, p = 0.70, list = FALSE)
+#cicids2017train <- cicids2017[ cicids2017trainIndex, ]
+#cicids2017test <- cicids2017[-cicids2017trainIndex, ]
+
+#trainCtrl <- trainControl(method = "cv", number = 10, verboseIter = TRUE)
+
+
+#svm.cicids2017Model <- train(Label~., data = cicids2017train, method = "svmRadial", tuneLength = 10, trControl = trainCtrl, metric = "Accuracy")
+
