72769ea5e94ce7c04818e88856fd142a28368373
Scott Duff <austin.duff@icloud.com>
2022-11-28 11:47:39 -0500

    Update SVM

--- CIC_IDS_2017_Final/1_1_Feature_Selection_Code.R
+++ CIC_IDS_2017_Final/1_1_Feature_Selection_Code.R
@@ -6,7 +6,7 @@ set.seed(4242)
 dataset <- read.csv("C:\\Users\\duffa\\OneDrive\\Documents\\GitHub\\WVU_CYBR_520_Group_1\\CIC_IDS_2017_Final\\MachineLearningCVE\\Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv", sep = ",")
 dataset$x <- NULL
 
-# Selects Columns that total Zero.
+# Remove columns that total zero (0), function.
 only_zeros <- function(x) {
   if(class(x) %in% c("integer", "numeric")) {
     all(x == 0, na.rm = TRUE) 
@@ -15,18 +15,16 @@ only_zeros <- function(x) {
   }
 }
 
-# Removes columns that total Zero
+# Apply the only_Zero function to the dataset.
 dataset_nozero <- dataset[ , !sapply(dataset, only_zeros)]
 dataset_backup <- dataset[ , !sapply(dataset, only_zeros)]
-
-#write.csv(dataset_nozero,"C:\\Users\\duffa\\Documents\\export.csv", row.names = FALSE)
 
 dataset_nozero[dataset_nozero =='BENIGN'] <- as.numeric(0)              
 dataset_nozero[dataset_nozero =='DDoS'] <- as.numeric(1)                 
 dataset_nozero$Label = as.numeric(dataset_nozero$Label)                    
 str(dataset_nozero)
 
-#correlationMatrix <- cor(dataset_nozero[,1:69])
+correlationMatrix <- cor(dataset_nozero[,1:69])
 
 print(correlationMatrix)
 print(correlationMatrix[69,])
@@ -44,13 +42,14 @@ set.seed(4242)
 
 control <- trainControl(method="repeatedcv", number=10, repeats=3)
 
-#model <- train(as.factor(Label)~., data=dataset_nozero, method="lvq", 
-#               preProcess="scale", trControl=control)
+model <- train(as.factor(Label)~., data=dataset_nozero, method="lvq", 
+               preProcess="scale", trControl=control)
 
-#importance <- varImp(model, scale=FALSE)
+importance <- varImp(model, scale=FALSE)
 
 print(importance)
 
+# Configure dataset with features selected in 1_0_Feature Selection_Code.R
 importantfeatures10 <- c("Bwd.Packet.Length.Mean", "Avg.Bwd.Segment.Size", 
                          "Bwd.Packet.Length.Max", "Bwd.Packet.Length.Std", 
                          "Destination.Port", "URG.Flag.Count", 
@@ -59,21 +58,4 @@ importantfeatures10 <- c("Bwd.Packet.Length.Mean", "Avg.Bwd.Segment.Size",
 
 datasetfeatures10 <- dataset_backup[importantfeatures10]
 
-# Above this is the default configuration. If you load the environment file you will not have to run these again.
-# Below this point is SVM
 
-#trainIndex <- createDataPartition(datasetfeatures10$Label, p = 0.70, list = FALSE)
-#train <- datasetfeatures10[ trainIndex, ]
-#test <- datasetfeatures10[-trainIndex, ]
-
-#trainCtrl <- trainControl(method = "cv", number = 10, verboseIter = TRUE)
-
-#svm.model <- train(Label~., data = train, method = "svmRadial", tuneLength = 10, trControl = trainCtrl, metric = "Accuracy")
-
-#svm.model$times
-#svm.model$results
-
-#svm.predict <- predict(svm.model, test)
-#modelpredict <- confusionMatrix(svm.predict, as.factor(test$Label), mode = "prec_recall")
-
-#modelpredict
---
+++ CIC_IDS_2017_Final/3_1_SVM_Code.R
@@ -0,0 +1,46 @@
+library(caret)
+library(e1071)
+
+set.seed(4242)
+
+dataset <- read.csv("C:\\Users\\duffa\\OneDrive\\Documents\\GitHub\\WVU_CYBR_520_Group_1\\CIC_IDS_2017_Final\\MachineLearningCVE\\Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv", sep = ",")
+dataset$x <- NULL
+
+# Remove columns that total zero (0), function.
+only_zeros <- function(x) {
+  if(class(x) %in% c("integer", "numeric")) {
+    all(x == 0, na.rm = TRUE) 
+  } else { 
+    FALSE
+  }
+}
+
+# Apply the only_Zero function to the dataset.
+dataset_nozero <- dataset[ , !sapply(dataset, only_zeros)]
+
+# Configure dataset with features selected in 1_0_Feature Selection_Code.R
+importantfeatures10 <- c("Bwd.Packet.Length.Mean", "Avg.Bwd.Segment.Size", 
+                         "Bwd.Packet.Length.Max", "Bwd.Packet.Length.Std", 
+                         "Destination.Port", "URG.Flag.Count", 
+                         "Packet.Length.Mean", "Average.Packet.Size", 
+                         "Packet.Length.Std", "Min.Packet.Length", "Label")
+
+# Dataset with selected features and features with columns with totals of zero (0).
+datasetfeatures10 <- dataset_nozero[importantfeatures10]
+
+#SVM Setup and Model Creation
+trainIndex <- createDataPartition(datasetfeatures10$Label, p = 0.70, list = FALSE)
+train <- datasetfeatures10[ trainIndex, ]
+test <- datasetfeatures10[-trainIndex, ]
+
+trainCtrl <- trainControl(method = "cv", number = 10, verboseIter = TRUE)
+
+svm.model <- train(type~., data = train, method = "svmRadial", tuneLength = 10, trControl = trainCtrl, metric = "Accuracy")
+
+svm.predict <- predict(svm.model, test)
+modelpredict <- confusionMatrix(svm.predict, as.factor(test$Label), mode = "prec_recall")
+
+modelpredict
+svm.predict
+svm.model$times
+svm.model$results
