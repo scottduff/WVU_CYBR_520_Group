82a59fe00dbcd26d31df9dc1a8950d509803bff5
ghislain11 <115494719+ghislain11@users.noreply.github.com>
2022-12-02 22:16:44 -0500

    Update Ghislain 2_1_Default_Code - Copy.R

--- CIC_IDS_2017_Final/Ghislain 2_1_Default_Code - Copy.R
+++ CIC_IDS_2017_Final/Ghislain 2_1_Default_Code - Copy.R
@@ -3,7 +3,7 @@ library(e1071)
 
 set.seed(4242)
 
-dataset <- read.csv("C:\\Users\\duffa\\OneDrive\\Documents\\GitHub\\WVU_CYBR_520_Group_1\\CIC_IDS_2017_Final\\MachineLearningCVE\\Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv", sep = ",")
+dataset <- read.csv("C:\\Users\\ajghi\\OneDrive\\Documents\\GitHub\\WVU_CYBR_520_Group_1\\CIC_IDS_2017_Final\\MachineLearningCVE\\Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv", sep = ",")
 dataset$x <- NULL
 
 # Remove columns that total zero (0), function.
@@ -27,4 +27,20 @@ importantfeatures10 <- c("Bwd.Packet.Length.Mean", "Avg.Bwd.Segment.Size",
 
 # Dataset with selected features and features with columns with totals of zero (0).
 datasetfeatures10 <- dataset_nozero[importantfeatures10]
-
+knn3.model <- train(Label~. , data=train, method="knn3",
+                   tuneLength = 10,
+                   trControl = trainctrl ,
+                   metric="Accuracy")
+rpart.model <- train(Label~., data=train, method="rpart", tuneLength =10, trControl = trainctrl , metric="Accuracy")
+install.packages("caret")
+library(caret)
+rpart.model <- train(Label~., data=train, method="rpart", tuneLength =10, trControl = trainctrl , metric="Accuracy")
+install.packages("earth")
+bagFDA.model <- train(Label~., data = train, method="bagFDA", tuneLength = 10, trControl = trainctrl , metric="Accuracy")
+install.packages("mda")
+library(earth)
+library(mda)
+bagFDA.model <- train(Label~., data = train, method="bagFDA", tuneLength = 10, trControl = trainctrl , metric="Accuracy")
+set.seed(4242)
+trainctrl <- trainControl(method = "cv", number = 10, verboseIter = TRUE)
+bagFDA.model <- train(Label~., data = train, method="bagFDA", tuneLength = 10, trControl = trainctrl , metric="Accuracy")
